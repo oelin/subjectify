848141b61d8114593ec049be98181bcfecf17caa
kale <42823429+oelin@users.noreply.github.com>
2023-02-28 11:26:29 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+export default subject => new Proxy(subject, {
+        get(_, key, __) {
+                if (typeof(subject[key]) === 'function') {
+                        return function(...a) {
+                                subject[key](...a)
+                                return this
+                        }
+                }
+        }
+})
